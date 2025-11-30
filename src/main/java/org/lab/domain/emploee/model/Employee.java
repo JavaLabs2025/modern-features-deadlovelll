f9d9636682a13c472d439f28748149f7b16d2e73
Timofey Ivankov <playboicartina@gmail.com>
2025-11-30 15:13:14 +0300

    impl to Employee domain model

--- src/main/java/org/lab/domain/emploee/model/Employee.java
+++ src/main/java/org/lab/domain/emploee/model/Employee.java
@@ -5,9 +5,10 @@ import java.util.Date;
 import lombok.Data;
 
 import org.lab.core.constants.employee.EmployeeType;
+import org.lab.domain.interfaces.DomainObject;
 
 @Data
-public class Employee {
+public class Employee implements DomainObject {
     private int id;
     private String name;
     private int age;
