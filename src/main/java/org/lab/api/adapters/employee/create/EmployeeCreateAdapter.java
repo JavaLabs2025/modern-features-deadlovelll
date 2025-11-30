511b7144c059e9911b2cd06430941554515fe081
Timofey Ivankov <playboicartina@gmail.com>
2025-11-30 16:36:09 +0300

    added exception handling

--- src/main/java/org/lab/api/adapters/employee/create/EmployeeCreateAdapter.java
+++ src/main/java/org/lab/api/adapters/employee/create/EmployeeCreateAdapter.java
@@ -1,5 +1,6 @@
 package org.lab.api.adapters.employee.create;
 
+import io.javalin.http.Context;
 import org.lab.core.utils.mapper.ObjectMapper;
 import org.lab.application.employee.dto.get.GetEmployeeDTO;
 import org.lab.application.employee.dto.create.CreateEmployeeDTO;
@@ -8,33 +9,36 @@ import org.lab.application.employee.use_cases.create.CreateEmployeeUseCase;
 import org.lab.domain.shared.exceptions.NotPermittedException;
 import org.lab.domain.shared.exceptions.UserAlreadyExistsException;
 
+import java.util.Map;
+
 public class EmployeeCreateAdapter {
 
     private final ObjectMapper mapper = new ObjectMapper();
     private final CreateEmployeeUseCase useCase = new CreateEmployeeUseCase();
 
-    public GetEmployeeDTO getEmployee(
-            CreateEmployeeDTO createEmployeeDTO
+    public Context getEmployee(
+            Context ctx
     ) {
         try {
-            Employee employee = mapper.mapToDomain(createEmployeeDTO, Employee.class);
+            CreateEmployeeDTO dto = ctx.bodyAsClass(CreateEmployeeDTO.class);
+            Employee employee = mapper.mapToDomain(dto, Employee.class);
             Employee createdEmployee = useCase.execute(
                     employee,
-                    createEmployeeDTO.creatorId()
+                    dto.creatorId()
             );
             GetEmployeeDTO presentationEmployee = mapper.mapToPresentation(
                     createdEmployee,
                     GetEmployeeDTO.class
             );
-            return presentationEmployee;
-
-        } catch (NotPermittedException e) {
-            System.err.println(e.getMessage());
-            return null;
+            return ctx.status(201).json(presentationEmployee);
 
         } catch (UserAlreadyExistsException e) {
-            System.err.println("user already exists!!!");
-            return null;
+            return ctx.status(409).json(Map.of("error", "User already exists"));
+
+        } catch (NotPermittedException e) {
+            return ctx.status(403).json(Map.of("error", e.getMessage()));
+        } catch (Exception e) {
+            return ctx.status(500).json(Map.of("error", "Internal server error"));
         }
     }
 }
