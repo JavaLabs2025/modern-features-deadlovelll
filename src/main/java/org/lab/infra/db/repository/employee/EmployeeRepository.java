c29eeb0571fbbbbf378d13c8c6ee86c927dc179a
Timofey Ivankov <playboicartina@gmail.com>
2025-11-30 14:56:58 +0300

    создан репозиторий сотрудников

---
+++ src/main/java/org/lab/infra/db/repository/employee/EmployeeRepository.java
@@ -0,0 +1,28 @@
+package org.lab.infra.db.repository.employee;
+
+import org.lab.domain.emploee.model.Employee;
+import org.lab.infra.db.client.DatabaseClient;
+
+public class EmployeeRepository {
+    private DatabaseClient databaseClient;
+
+    public EmployeeRepository() {
+        databaseClient = new DatabaseClient();
+    }
+
+    public Employee getById(int id) {
+        return new Employee();
+    }
+
+    public Employee create(String name) {
+        return new Employee();
+    }
+
+    public Object delete(int id) {
+        return null;
+    }
+
+    public Employee update(Employee employee) {
+        return null;
+    }
+}
