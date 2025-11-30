3698964a84a02974d05f4579aaccddbda3d3c0cf
Timofey Ivankov <playboicartina@gmail.com>
2025-11-30 14:52:25 +0300

    db client

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,17 @@ repositories {
 }
 
 dependencies {
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+    implementation("org.postgresql:postgresql:42.7.8")
+    implementation("com.zaxxer:HikariCP:5.0.1")
+
+    compileOnly("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation("io.javalin:javalin:6.1.3")
-    implementation("ch.qos.logback:logback-classic:1.4.11")
-    compileOnly("org.projectlombok:lombok:1.18.32")
-    annotationProcessor("org.projectlombok:lombok:1.18.32")
     testCompileOnly("org.projectlombok:lombok:1.18.32")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.32")
 }
--- src/main/java/org/lab/infra/db/client/DatabaseClient.java
+++ src/main/java/org/lab/infra/db/client/DatabaseClient.java
@@ -1,4 +1,26 @@
 package org.lab.infra.db.client;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DatabaseClient {
+    private static HikariDataSource dataSource;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:postgresql://postgres:5432/mydb");
+        config.setUsername("postgres");
+        config.setPassword("postgres");
+        config.setMaximumPoolSize(10); // размер пула
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static Connection getConnection()
+            throws SQLException
+    {
+        return dataSource.getConnection();
+    }
 }
